a129b7bc6bd335cd799a47b2a4445f25247d3073
Daniel Whymark <13569578+daniel-whymark@users.noreply.github.com>
2022-07-07 14:02:26 +0100

    Panelbear test #2

---
+++ assets/js/panelbear.js
@@ -0,0 +1,8 @@
+window.panelbear = window.panelbear || function() {
+	(window.panelbear.q = window.panelbear.q || []).push(arguments);
+};
+panelbear('config', {
+	site: '1zP4ZSY87Mx',
+    spaMode: 'history',
+    debug: false,
+});
